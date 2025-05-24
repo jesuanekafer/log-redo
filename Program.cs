cd1750fde0585132b1a1b9ca4661a71bca203dd5
Jesuane Kafer <Jesuane Kafer@JESUANE>
2025-05-24 16:24:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace trabalho_bd_log
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
