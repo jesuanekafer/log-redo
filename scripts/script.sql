ef583e0410b4b233ff7b69e3194a0063110644e3
Jesuane Kafer <jesuanekafer2@gmail.com>
2025-05-25 16:58:54 -0300

    ajustanco scipts

---
+++ scripts/script.sql
@@ -0,0 +1,42 @@
+
+--CREATE UNLOGGED TABLE clientes_em_memoria (
+--  id SERIAL PRIMARY KEY,
+--  nome TEXT,
+--  saldo NUMERIC
+--);
+
+--CREATE TABLE log (
+--	id SERIAL PRIMARY KEY
+--	operacao TEXT, 
+--	id_cliente INT, 
+--	nome TEXT, 
+--	saldo numeric
+--);
+
+
+--CREATE OR REPLACE FUNCTION gravar_log() RETURNS TRIGGER AS $$
+--BEGIN
+--  IF TG_OP = 'INSERT' THEN
+--    INSERT INTO log (operacao, id_cliente, nome, saldo)
+--    VALUES ('INSERT', NEW.id, NEW.nome, NEW.saldo);
+--    RETURN NEW;
+
+--  ELSIF TG_OP = 'UPDATE' THEN
+--    INSERT INTO log (operacao, id_cliente, nome, saldo)
+--    VALUES ('UPDATE', NEW.id, NEW.nome, NEW.saldo);
+--    RETURN NEW;
+
+--  ELSIF TG_OP = 'DELETE' THEN
+--    INSERT INTO log (operacao, id_cliente, nome, saldo)
+--    VALUES ('DELETE', OLD.id, OLD.nome, OLD.saldo);
+--    RETURN OLD;
+--  END IF;
+
+--  RETURN NULL;
+--END;
+--$$ LANGUAGE plpgsql;
+
+
+--CREATE TRIGGER trg_log_clientes
+--AFTER INSERT OR UPDATE OR DELETE ON clientes_em_memoria
+--FOR EACH ROW EXECUTE FUNCTION gravar_log();
---
+++ scripts/transações.sql
@@ -0,0 +1,21 @@
+
+--BEGIN;
+--	INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 1', 100.00);
+--	UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 1;
+--END;
+
+--BEGIN;
+--	INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 2', 200.00);
+--	UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 2;
+--END;
+--BEGIN;
+--	INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 3', 300.00);
+--	UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 2;
+--END;
+
+--BEGIN;
+--	INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 4', 400.00);
+--	UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 3;
+
+--BEGIN;
+--INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 6', 600.00); 
