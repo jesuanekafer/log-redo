33bb0be9e4bbf5ec5df7532706d367badb267c96
Jesuane Kafer <jesuanekafer2@gmail.com>
2025-05-25 17:03:48 -0300

    classe Log com estrutura da tabela para armazenar seus dados

---
+++ src/Log.cs
@@ -0,0 +1,12 @@
+﻿namespace trabalho_bd_log.src
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Operacao { get; set; }
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public decimal? Saldo { get; set; }
+    }
+
+}
