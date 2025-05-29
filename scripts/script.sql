dec3c67b301e81b1c87bb23718effd3240b0b4d0
Jesuane Kafer <jesuanekafer2@gmail.com>
2025-05-29 17:24:30 -0300

    atualizando scripts para inserir begin e end

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,34 +1,34 @@
-
 --CREATE UNLOGGED TABLE clientes_em_memoria (
---  id SERIAL PRIMARY KEY,
---  nome TEXT,
---  saldo NUMERIC
+--   id SERIAL PRIMARY KEY,
+--   nome TEXT,
+--   saldo NUMERIC
+
 --);
 
 --CREATE TABLE log (
---	id SERIAL PRIMARY key,
+--	txid BIGINT,
 --	operacao TEXT, 
 --	id_cliente INT, 
 --	nome TEXT, 
---	saldo numeric
+--	saldo NUMERIC
 --);
 
 
 --CREATE OR REPLACE FUNCTION gravar_log() RETURNS TRIGGER AS $$
 --BEGIN
 --  IF TG_OP = 'INSERT' THEN
---    INSERT INTO log (operacao, id_cliente, nome, saldo)
---    VALUES ('INSERT', NEW.id, NEW.nome, NEW.saldo);
+--    INSERT INTO log (txid, operacao, id_cliente, nome, saldo)
+--    VALUES (txid_current(), 'INSERT', NEW.id, NEW.nome, NEW.saldo);
 --    RETURN NEW;
 
 --  ELSIF TG_OP = 'UPDATE' THEN
---    INSERT INTO log (operacao, id_cliente, nome, saldo)
---    VALUES ('UPDATE', NEW.id, NEW.nome, NEW.saldo);
+--    INSERT INTO log (txid, operacao, id_cliente, nome, saldo)
+--    VALUES (txid_current(), 'UPDATE', NEW.id, NEW.nome, NEW.saldo);
 --    RETURN NEW;
 
 --  ELSIF TG_OP = 'DELETE' THEN
---    INSERT INTO log (operacao, id_cliente, nome, saldo)
---    VALUES ('DELETE', OLD.id, OLD.nome, OLD.saldo);
+--    INSERT INTO log (txid, operacao, id_cliente, nome, saldo)
+--    VALUES (txid_current(), 'DELETE', OLD.id, OLD.nome, OLD.saldo);
 --    RETURN OLD;
 --  END IF;
 
@@ -40,3 +40,28 @@
 --CREATE TRIGGER trg_log_clientes
 --AFTER INSERT OR UPDATE OR DELETE ON clientes_em_memoria
 --FOR EACH ROW EXECUTE FUNCTION gravar_log();
+
+
+
+--CREATE OR REPLACE FUNCTION inserir_begin_em_log() RETURNS void AS $$
+--BEGIN 
+--  INSERT INTO log (txid, operacao) 
+--  VALUES (txid_current(), 'BEGIN');
+--END;
+--$$ LANGUAGE plpgsql;
+
+
+--CREATE OR REPLACE FUNCTION inserir_end_em_log() RETURNS void AS $$
+--BEGIN 
+--  INSERT INTO log (txid, operacao) 
+--  VALUES (txid_current(), 'END');
+--END;
+--$$ LANGUAGE plpgsql;
+
+--CREATE OR REPLACE FUNCTION inserir_rollback_em_log() RETURNS void AS $$
+--BEGIN 
+--  INSERT INTO log (txid, operacao) 
+--  VALUES (txid_current(), 'ROLLBACK');
+--END;
+--$$ LANGUAGE plpgsql;
+
--- scripts/transações.sql
+++ scripts/transações.sql
@@ -1,19 +1,38 @@
 
---BEGIN;
---INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 1', 100.00);
---UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 1;
---END;
---BEGIN;
---INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 2', 200.00);
---UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 2;
---END;
---BEGIN;
---INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 3', 300.00);
---UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 2;
---END;
---BEGIN;
---INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 4', 400.00);
---UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 3;
+--begin;
+--	select inserir_begin_em_log();
+--	insert into clientes_em_memoria (nome, saldo) values ('cliente 1', 100.00);
+--	update clientes_em_memoria set saldo = saldo + 50 where id = 1;
+--	select inserir_end_em_log();
+--end;
 
---BEGIN;
---INSERT INTO clientes_em_memoria (nome, saldo) VALUES ('Cliente 6', 600.00); 
+
+
+--begin;
+--	select inserir_begin_em_log();
+--	insert into clientes_em_memoria (nome, saldo) values ('cliente 2', 200.00);
+--	update clientes_em_memoria set saldo = saldo + 50 where id = 2;
+--	select inserir_end_em_log();
+--end;
+
+
+
+--begin;
+--	select inserir_begin_em_log();
+--	insert into clientes_em_memoria (nome, saldo) values ('cliente 3', 300.00);
+--	update clientes_em_memoria set saldo = saldo + 50 where id = 2;
+--	select inserir_end_em_log();
+--end;
+
+
+--begin;
+--	select inserir_begin_em_log();
+--	insert into clientes_em_memoria (nome, saldo) values ('cliente 4', 400.00);
+--	update clientes_em_memoria set saldo = saldo + 50 where id = 3;
+--	select inserir_rollback_em_log();
+	
+
+--begin;
+--	select inserir_begin_em_log();
+--    insert into clientes_em_memoria (nome, saldo) values ('cliente 6', 600.00); 
+--	select inserir_rollback_em_log();
